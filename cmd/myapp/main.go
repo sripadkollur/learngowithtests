cf30729a86c7f91752b4cf7786a2bca980c3148a
Sripadwallabha D. Kollur <sripadwallabha.d.kollur@hpe.com>
2021-07-17 22:50:43 +0530

    multiple chapeters

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"time"
 
-	"github.com/sripadkollur/learngowithtests/pkg/integers"
+	"github.com/sripadkollur/learngowithtests/pkg/mocking"
 )
 
+// func Greet(writer io.Writer, name string) {
+// 	fmt.Fprintf(writer, "Hello, %s", name)
+// }
+
+// func Countdown(out io.Writer) {
+// 	fmt.Fprintf(out, "3")
+// }
 func main() {
-	fmt.Println(integers.Add(2, 2))
+	// fmt.Println(integers.Add(2, 2))
+	// Greet(os.Stdout, "Elodie")
+	// di.Greet(os.Stdout, "Sripad")
+
+	sleeper := &mocking.ConfigurableSleeper{Duration: 2 * time.Second, CallSleep: time.Sleep}
+	mocking.Countdown(os.Stdout, sleeper)
 }
